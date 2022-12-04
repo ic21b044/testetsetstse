95894815de3f19b9749305f07c84d12cb0b8c87c
ic21b044@technikum-wien.at <nbwhiDZJaYRxVGTu6Lgs>
2022-12-04 14:55:03 +0100

    Initial commit

---
+++ src/main/java/damo/fh/vorbereitung/VorbereitungApplication.java
@@ -0,0 +1,13 @@
+package damo.fh.vorbereitung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VorbereitungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VorbereitungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/damo/fh/vorbereitung/VorbereitungApplicationTests.java
@@ -0,0 +1,13 @@
+package damo.fh.vorbereitung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VorbereitungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
